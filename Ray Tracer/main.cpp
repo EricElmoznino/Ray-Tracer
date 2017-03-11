fc5de26fd91bd51ad9eee6e43557ed8a15986e5d
Eric <eric.elmoznino@gmail.com>
2017-03-11 17:32:36 -0500

    Initial Commit

---
+++ Ray Tracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ray Tracer
+//
+//  Created by Eric Elmoznino on 2017-03-11.
+//  Copyright © 2017 Eric Elmoznino. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
